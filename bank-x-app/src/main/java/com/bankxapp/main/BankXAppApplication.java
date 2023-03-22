d5948803d77b9c8a7d6c81a76098ad704be4fad9
SaikumarPeddi <peddisaikumarsai@gmail.com>
2023-03-22 23:05:59 +0530

    Adding pom.xml file

---
+++ bank-x-app/src/main/java/com/bankxapp/main/BankXAppApplication.java
@@ -0,0 +1,13 @@
+package com.bankxapp.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankXAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankXAppApplication.class, args);
+    }
+
+}
---
+++ bank-x-app/src/test/java/com/bankxapp/Entity/BankXAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankxapp.Entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankXAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
